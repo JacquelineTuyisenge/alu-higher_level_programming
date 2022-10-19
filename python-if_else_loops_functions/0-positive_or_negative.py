d8c5797d90372d73367e5a406847353e90c44945
root <j.tuyisenge@alustudent.com>
2022-10-19 06:56:05 -0700

    integer count

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+
 def pnive(x):
+
 if x > 0:
-    print(f"{x} is positive\n")
-elif x == 0:
-    print(f"{x} is zero\n")
-else: x < 0:
-    print(f"{x} is negative\n")
+    print(f"{x} is positive")
+elif x < 0:
+    print(f"{x} is negative")
+else:
+    print(f"{x} is negative")
+
 pnive(number)
