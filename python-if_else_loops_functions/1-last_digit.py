7110ce753b964a32e4350b1849c6b5dca840bf47
root <j.tuyisenge@alustudent.com>
2022-10-19 08:06:23 -0700

    integer counting

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,19 +3,19 @@ import random
 number = random.randint(-10000, 10000)
 
 
-def pnivel(string):
-    
-    if string >= 0:
-        x = string % 10
+def pnivel(st):
+
+    if st >= 0:
+        x = st % 10
         if x > 5:
-            print(f"Last digit of {string} is {x} and is greater than 5")
+            print(f"Last digit of {st} is {x} and is greater than 5")
         elif x == 0:  
-            print(f"Last digit of {string} is {x} and is 0")
+            print(f"Last digit of {st} is {x} and is 0")
         elif x < 6 & x != 0:
-            print(f"Last digit of {string} is {x} and is less than 6 and not o")
+            print(f"Last digit of {st} is {x} and is less than 6 and not o")
     else:
         x = -string % 10
         y = -x
-        print(f"Last digit of {string} is {y} and is less than 6 and not 0")
+        print(f"Last digit of {st} is {y} and is less than 6 and not 0")
 
 pnivel(number)
