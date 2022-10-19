199cb9f459d022d778dfd40beb2b92ebb8328dba
root <j.tuyisenge@alustudent.com>
2022-10-19 08:26:10 -0700

    integer

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,14 +8,14 @@ def pnivel(st):
     if st >= 0:
         x = st % 10
         if x > 5:
-            print(f"Last digit of {st} is {x} and is greater than 5")
+            print(f" Last digit of {st} is {x} and is greater than 5")
         elif x == 0:  
-            print(f"Last digit of {st} is {x} and is 0")
+            print(f" Last digit of {st} is {x} and is 0")
         elif x < 6 & x != 0:
-            print(f"Last digit of {st} is {x} and is less than 6 and not o")
+            print(f" Last digit of {st} is {x} and is less than 6 and not o")
     else:
         x = -st % 10
         y = -x
-        print(f"Last digit of {st} is {y} and is less than 6 and not 0")
+        print(f" Last digit of {st} is {y} and is less than 6 and not 0")
 
 pnivel(number)
