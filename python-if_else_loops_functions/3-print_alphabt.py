e42e1a574f701bfa4fea39fbb92c6adab79acfda
root <j.tuyisenge@alustudent.com>
2022-10-19 15:25:02 -0700

    alphabets

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-   if i == 101 and i == 113:
-       continue
-   else:
+    if i == 101 or i == 113:
+        continue
+    else:
         print("{}".format(chr(i)), end='')
