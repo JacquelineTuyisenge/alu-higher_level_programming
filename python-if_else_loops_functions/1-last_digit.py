c31977a1b5ca8e594de4880d43bdc146a8c1ea94
root <j.tuyisenge@alustudent.com>
2022-10-19 08:01:07 -0700

    integer count

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,14 +8,14 @@ def pnivel(string):
     if string >= 0:
         x = string % 10
         if x > 5:
-            print(f"last digit of {string} is {x} and is greater than 5")
+            print(f"Last digit of {string} is {x} and is greater than 5")
         elif x == 0:  
-            print(f"last digit of {string} is {x} and is 0")
+            print(f"Last digit of {string} is {x} and is 0")
         elif x < 6 & x != 0:
-            print(f"last digit of {string} is {x} and is less than 6 and not o")
+            print(f"Last digit of {string} is {x} and is less than 6 and not o")
     else:
         x = -string % 10
         y = -x
-        print(f"last digit of {string} is {y} and is less than 6 and not 0")
+        print(f"Last digit of {string} is {y} and is less than 6 and not 0")
 
 pnivel(number)
