6863863ac9028f32abce69da78f2f089f5939dfe
root <j.tuyisenge@alustudent.com>
2022-10-21 13:08:35 -0700

    two digits number

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-    print("{}".format(i==99), end=' ')
+for i in range(0, 100):
+    if i == 99:
+        print(i)
+    else:
+        print("{:02d}".format(i), end=', ')
+    
