56ff9da94144b47c33adaf5e3dea547cc87e8595
root <j.tuyisenge@alustudent.com>
2022-10-23 16:36:15 -0700

    maths

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,14 +3,6 @@ from calculator_1 import maths
 if __name__ == "__main__":
     a = 10
     b = 5
-    add = a + b
-        return (add)
-    sub = a - b
-        return (sub)
-    mul = a * b
-        return (mul)
-    div = a / b
-        return (div)
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
