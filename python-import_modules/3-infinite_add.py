44804e2481dc69719a39d64b58dee86bb26a1635
root <j.tuyisenge@alustudent.com>
2022-10-23 16:59:23 -0700

    mandatory tasks

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    length = len(argv)
+    sum = 0
+    for x in range(1, length):
+        sum += int(argv[x])
+    print("{:d}".format(sum))    
---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    for str in dir(hidden_4):
+        if str[:2] != "__":
+            print("{}".format(str))
