e5c969e3fd269801e623bcaccd4a77650fd54f92
root <j.tuyisenge@alustudent.com>
2022-10-23 16:23:07 -0700

    maths

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import maths
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    sum = a + b:
+        return (sum)
+    subtraction = a - b:
+        return (subtraction)
+    multiplication = a * b:
+        return (multiplication)
+    division = a / b:
+        return (division)
+    print("{} {} = {}".format(a, b), end="")
