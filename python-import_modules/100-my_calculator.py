c1e55fe4197e160bd4db80536a7c982be3e8b975
root <j.tuyisenge@alustudent.com>
2022-10-23 17:39:13 -0700

    advanced tasks

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    number = len(argv)
+
+    if number != 4:
+        print"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    c = argv[2]
+    b = int(argv[3])
+
+    if c == "+":
+        num = add(a, b)
+    elif c == "-":
+        num = sub(a, b)
+    elif c == "/":
+        num = div(a, b)
+    elif c == "*":
+        num = mul(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, op, b, num))
+        
