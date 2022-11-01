7533c19d7c16e994b72dd00b18f47c6cc9bc519f
root <j.tuyisenge@alustudent.com>
2022-11-01 15:02:12 -0700

    python3

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0 or idx > (len(my_list) - 1):
-            return None
-        else:
-            return my_list[idx]
+    if idx < 0 or idx > (len(my_list) -1):
+        return None
+    return (my_list[idx])
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for idx in my_list:
-        if idx < 0 or idx > (len(my_list)):
-            return no modification
-        else:
-            return my_list[idx(element)]
+    my_list[idx] = element
+    if idx > o or idx > (len(my_list) -1):
+        return none
+    return (my_list)
