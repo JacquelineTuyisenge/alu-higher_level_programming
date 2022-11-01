ace5c92bfc85f91af1518cbe5d90bf5891357b7b
root <j.tuyisenge@alustudent.com>
2022-11-01 14:43:03 -0700

    python

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for idx in my_list:
+        if idx < 0 or idx > (len(my_list)):
+            return no modification
+        else:
+            return my_list[idx(element)]
