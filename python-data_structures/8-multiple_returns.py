6fbe0da0f5d788bbf76c84b595fe379752453e10
root <j.tuyisenge@alustudent.com>
2022-11-02 14:06:42 -0700

    strucures

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
    for x in sentences:
-       if len(x) = []:
+       if len(x) == []:
            return None
        else:
-           return sentences
+           return sentence
