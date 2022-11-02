c0116c448328875108dbcc9936761236896b21f2
root <j.tuyisenge@alustudent.com>
2022-11-02 14:44:49 -0700

    data structure

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = []
     for i in my_list:
-        i = len(my_list)
         if i % 2 == 0:
-            return True
+            new_list.append(True)
         else:
-            return False
+            new_list.append(False)
+    return new_list       
