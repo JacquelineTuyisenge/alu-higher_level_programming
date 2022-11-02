c8c485cbe98dab770479e70fb21cff7f914074c6
root <j.tuyisenge@alustudent.com>
2022-11-02 10:02:15 -0700

    data structure

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    for letter in my_string:
+        letter = [65:123]
+        if letter[67:68] = [] or letter[99:100] = []:
+            return my_string
