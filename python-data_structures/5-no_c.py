ac165cba6f95193ca4c2d6e7f7e27595b7380888
root <j.tuyisenge@alustudent.com>
2022-11-02 13:46:07 -0700

    data structure

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    {ord('c'): None, ord('C'): None}
-    for i in my_string:
-        if ord('i') == 99 or 67:
-            my_string.translate({ord('c'): None, ord('C'): None})
-    return my_string.translate({ord('c'): None, ord('C'): None})
+    copy = [letter for letter in my_string if letter != 'c' and letter != 'C']
+    return ("".join(copy))
