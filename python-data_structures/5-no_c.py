bf4a2f4eeac52c83a703db0d413ba62f4075d274
root <j.tuyisenge@alustudent.com>
2022-11-02 11:45:04 -0700

    data structure

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     {ord('c'): None, ord('C'): None}
     for n in my_string:
-        if ord('n') == 67 or 99:
+        if ord('n') == 99 or 67:
             my_string.translate({ord('c'): None, ord('C'): None})
-            return my_string.translate({ord('c'): None, ord('C'): None})
+            return my_string.translate({ord('c'): None, ord('C'): None)
