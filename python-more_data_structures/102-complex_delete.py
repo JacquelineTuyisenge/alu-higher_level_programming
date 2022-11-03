fc54560f135577c9c1b661d771984493aba0cee1
root <j.tuyisenge@alustudent.com>
2022-11-03 12:47:09 -0700

    advanced tasks

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    while value in a_directionary.values():
+        for k, v in a_dictionary.items():
+            if v == values:
+                del a_dictionary[k]
+                break
+    return (a_dictionary)        
