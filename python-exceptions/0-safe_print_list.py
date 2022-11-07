99107301a8a28b41d07ef99a99ad836025e914c2
root <j.tuyisenge@alustudent.com>
2022-11-06 17:54:24 -0800

    lis printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for x in  my_list:
     try:
-        f()
-    print x.index(x)
+        x >= my_list
+    except KeyError:
+        print(x)
