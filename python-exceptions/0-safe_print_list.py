dbc2fc115953884e80215308f460709964e12cdb
root <j.tuyisenge@alustudent.com>
2022-11-08 01:10:31 -0800

    list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     y = 0
     for n in range(x):
         try:
-            print("{}".format(my_list[n], end="")
+            print("{}".format(my_list[n]), end="")
             y += 1
         except IndexError:
             break
