14e36de9fc95a24e06f4d5e61513fc39d3b4dc9b
root <j.tuyisenge@alustudent.com>
2022-11-08 01:18:53 -0800

    print integers

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/python3
+def safe_print_integer(value):
+    for x in value:
+    try:
+        print("{:d}".format(valu[x])
+        return (True)
+        except (TypeError, ValueError):
+            return (False)
