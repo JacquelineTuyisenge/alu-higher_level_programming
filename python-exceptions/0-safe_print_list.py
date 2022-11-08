1a47a60f160bf94a335c9384569b4de5b378d588
root <j.tuyisenge@alustudent.com>
2022-11-08 01:05:08 -0800

    list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        my_list = [1,2,3,4]
-    except IndexError:
-        print("Sorry that index doesn't exist")
-    except:
-        print("An unknown error occured")
-    for n in my_list:
-        return my_list[n]
+    y = 0
+    for n in range(x):
+        try:
+            print("{}".format(my_list[n], end="")
+            y += 1
+        except IndexError:
+            break
+    print("")
+    return (y)
