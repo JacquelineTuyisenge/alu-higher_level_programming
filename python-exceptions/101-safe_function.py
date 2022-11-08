185fe9dc63299a77d96e1e2df46382cfcfb3f70d
root <j.tuyisenge@alustudent.com>
2022-11-08 13:00:39 -0800

    writting executed functions

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -5,9 +5,9 @@ import sys
 
 
 def safe_function(fct, *args):
-        try:
-           result = fct(*args)
-           return (result)
-        except:
-            print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-            return (None)
+    try:
+        result = fct(*args)
+        return (result)
+    except:
+          print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+          return (None)
