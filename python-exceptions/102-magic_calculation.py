aac6ca213176f0de24828ade51edbb4fd0b59b28
root <j.tuyisenge@alustudent.com>
2022-11-08 13:38:18 -0800

    bytecode writting

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if  i > a:
+            if i > a:
                 raise Exception('Too far')
             else:
                 result += a ** b / i
