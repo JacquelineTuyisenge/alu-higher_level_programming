badcc23639212dc3415ed15de29dcce9c31c32ce
root <j.tuyisenge@alustudent.com>
2022-11-10 13:41:52 -0800

    classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,17 @@
-#!/usr/bin/pytho3
+#!/usr/bin/python3
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-    Args:
-        size (int): size of the new square.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size    
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
