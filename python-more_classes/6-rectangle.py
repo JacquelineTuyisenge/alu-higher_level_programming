eefbd48dd88187d2e77376215236367b250e5440
root <j.tuyisenge@alustudent.com>
2022-11-11 06:12:39 -0800

    how many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = 
+        self.__width = value
 
     @property
     def height(self):
