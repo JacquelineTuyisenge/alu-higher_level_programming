7081e0d6aa8bd4194c813165931d57f023026bfd
root <j.tuyisenge@alustudent.com>
2022-11-10 22:22:48 -0800

    class and objects

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,7 +34,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tupple) or
+        if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
