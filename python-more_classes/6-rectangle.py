ef51ce92195247fd6c1b26b286ecc32411e6b268
root <j.tuyisenge@alustudent.com>
2022-11-11 06:15:06 -0800

    how many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -63,12 +63,12 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-         rect = []
-         for i in range(self.__height):
-             [rect.append('#') for j in range(self.__width)]
-             if i != self.__height - 1:
-                 rect.append("\n")
-         return ("".join(rect))
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
