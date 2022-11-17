6e8998ce1dcb91078fc3c70fd1eceb5fcbad9982
root <j.tuyisenge@alustudent.com>
2022-11-17 05:06:08 -0800

    mandatory task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# class basegeomertry
+"""
+    define a class 'BaseGeometry'
+"""
+
+
+class BaseGeometry:
+    """
+        Representation of 'BaseGeometry'
+    """
+
+    def area(self):
+        """
+            raises Exception with message
+        """
+        raise Exception('area() is not implemented')
