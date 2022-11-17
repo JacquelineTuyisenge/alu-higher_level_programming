204c107e5b6539bcef7ebf548d71fd3db5ab1133
root <j.tuyisenge@alustudent.com>
2022-11-17 11:55:34 -0800

    mandatory task

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,41 +2,22 @@
 # function def pascal_triangle(n):
 # that returns a list of lists of integers representing the
 # Pascal’s triangle of 'n':
-"""
-    class 'Student'
-"""
+"""define a function pascal_triangle."""
 
 
-class Student:
-    """
-        define class 'Student'
-    """
+def pascal_triangle(n):
+    """return the pascal triangle."""
 
-    def __init__(self, first_name, last_name, age):
-        """
-            init a new Student
-        """
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """
-            retrieve dictionary representation of a Student instance
-        """
-
-        if attrs is None or type(attrs) is not list:
-            return self.__dict__
-        for att in attrs:
-            if type(att) is not str:
-                return self.__dict__
-        return {x: self.__dict__[x] for x in self.__dict__ if x in attrs}
-
-    def reload_from_json(self, json):
-        """
-            replace all attributes of Student instance
-        """
-
-        for key, value in json.items():
-            self.__dict__[key] = value
+    if n <= 0:
+        return []
+    p_t = [[] for x in range(n)]
+    p_t[0] = [1]
+    if n > 1:
+        p_t[1] = [1, 1]
+    if n > 2:
+        for i in range(2, n):
+            p_t[i].append(1)
+            for j in range(i - 1):
+                p_t[i].append(p_t[i - 1][j] + p_t[i - 1][j + 1])
+            p_t[i].append(1)
+    return p_t
