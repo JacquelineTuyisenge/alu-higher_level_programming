4ffa1c241e530d8be518f0ae8cc785070091903d
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2022-11-18 13:09:25 +0000

    mandatory

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script takes in URL and displays all HTTP methods the server accept
-curl -sI "$1" | grep "Allow:" | cut -d "" -f2-
+curl -sI "$1" | grep "Allow:" | cut -d " " -f2-
