58a2d76e4b076e55a58736ed0408a6059c057fd4
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2022-11-22 15:18:10 +0000

    fetch

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode(utf8-8")))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
