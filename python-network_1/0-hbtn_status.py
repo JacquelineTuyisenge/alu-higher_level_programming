dd94218d3eea0b9543c4c2272fdbe91894f7f85c
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2022-11-22 15:22:20 +0000

    fetch

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alu-intranet.hbtn.io/status")
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
