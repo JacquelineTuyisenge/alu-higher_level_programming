812df857742589fef8187fc0b11c2ddf3ba3b10e
root <j.tuyisenge@alustudent.com>
2022-11-24 05:44:20 -0800

    priviledge

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- script that lists all privilages of the MYSQL users
+-- -- 'user_0d_1' and 'user_0d_2' on your server (in 'localhost')
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
