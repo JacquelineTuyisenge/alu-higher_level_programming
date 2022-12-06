7e999dd3ca5b0b08506b08c433d61db417e638fe
root <j.tuyisenge@alustudent.com>
2022-12-06 06:13:38 -0800

    add two integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# a function that adds two integers
+"""Define 'add_integer' function."""
+
+
+def add_integer(a, b=98):
+    """Return: Sum of integers a & b."""
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+readme file for pyhton test driven development
