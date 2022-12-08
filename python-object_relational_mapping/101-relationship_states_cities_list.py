d6d51d818d8b121fb9aa22d3fabb9005d2316282
root <j.tuyisenge@alustudent.com>
2022-12-08 05:28:01 -0800

    advanced last python project

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-# script that lists all State objects, and corresponding City objects,
-# contained in the database hbtn_0e_101_usa
+# script that lists all 'State' objects, and corresponding 'City' objects,
+# contained in the database 'hbtn_0e_101_uss'
 """import 'sys','sqlalchemy' & 'relationship_..'."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import State
-from relationship_city import Base, City
+from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.add(City(name="San Francisco", state=State(name="California")))
-    session.commit()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
