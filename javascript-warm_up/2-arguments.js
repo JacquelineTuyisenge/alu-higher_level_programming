f5e02fd0ac49ab60caa99ea4da60452c94ab33dd
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-02-01 23:06:42 +0200

    Create 2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let args = [1, 2];
+if (args == 1) {
+  console.log('No argument');
+} else if (args == 2) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
