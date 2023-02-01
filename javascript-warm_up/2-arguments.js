60c3739dc72cbeb77f4e0fe057707d0b5ba340d7
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-02-01 23:11:43 +0200

    jscript

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+const args = [1, 2];
 
-let args = [1, 2];
-if (args == 1) {
+if (args === 1) {
   console.log('No argument');
-} else if (args == 2) {
+} else if (args === 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
