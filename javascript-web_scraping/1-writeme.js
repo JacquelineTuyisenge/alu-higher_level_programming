9d615eeb2472e2e8d8c77518a1f918639f40ba53
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-02-06 22:07:26 +0200

    web scrap

--- javascript-web_scraping/1-writeme.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], 'utf-8', function (error, content) {
-  console.log(error || content);
-});
--- javascript-web_scraping/3-starwars_title.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const req = require('request');
-const URL = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
-req.get(URL, (err, res) => {
-  err ? console.log(err) : console.log(JSON.parse(res.body).title);
-});
