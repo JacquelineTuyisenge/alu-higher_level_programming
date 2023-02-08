7a2c4fd666dd5db8cc0b64a448c12754fd7f0b45
JacquelineTuyisenge <109481899+JacquelineTuyisenge@users.noreply.github.com>
2023-02-09 00:44:56 +0200

    Update 7-script.js

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function(data) {
-  $('UL#list_movies').text(data.name);
+  $('div#character').text(data.name);
 });
