815c76c7d9115e50e8c1edff3808499684fa751f
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-02-10 20:44:21 +0200

    javascripting

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
+let x = process.argv[2];
+let i = 0;
 
-if (isNaN(x)) {
+if (!Number.isInteger(Number(x))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  x = Number(x);
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
